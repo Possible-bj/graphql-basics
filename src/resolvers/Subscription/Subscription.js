84b61571c4e8d4ae8ca159544f14ca8dd701f035
Possible <ahunanyabenjamin@gmail.com>
2024-03-21 13:33:54 +0100

    All Done

---
+++ src/resolvers/Subscription/Subscription.js
@@ -0,0 +1,39 @@
+const Subscription = {
+  count: {
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0;
+
+      setInterval(() => {
+        count++;
+
+        pubsub.publish("count", {
+          count,
+        });
+      }, 1000);
+
+      return pubsub.asyncIterator("count");
+    },
+  },
+
+  comment: {
+    subscribe(parent, { postId }, { db, pubsub }, info) {
+      const postExist = db.posts.some(
+        (post) => post.id === postId && post.published
+      );
+
+      if (!postExist) {
+        throw new Error("Post Not Found!");
+      }
+
+      return pubsub.asyncIterator(`comment ${postId}`);
+    },
+  },
+
+  post: {
+    subscribe(parent, args, { pubsub }, info) {
+      return pubsub.asyncIterator(`post`);
+    },
+  },
+};
+
+export default Subscription;
--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -3,12 +3,15 @@ import { Mutation } from "./Mutation";
 import { User } from "./User";
 import { Post } from "./Post";
 import { Comment } from "./Comment";
+import { Subscription } from "./Subscription";
 
 export const resolvers = {
   // Query Resolver
   Query,
   //   Mutation
   Mutation,
+  //   Subscription
+  Subscription,
   //   Relational Data resolver
   User,
   Post,
